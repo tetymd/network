b3f599b778338b6b6be3b60d3875a34c0f087b8c
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2018-09-05 21:21:34 +0900

    handlerを追加

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+    "fmt"
     "net/http"
 )
 
 func main() {
+    http.HandleFunc("/", handler)
     http.ListenAndServe(":8000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello World fron Go!")
 }
